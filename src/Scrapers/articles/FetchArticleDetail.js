9f6eceeec94fbc8951b87b47ff150e02414c4e46
Rynare <rynare.of.legend@gmail.com>
2024-05-29 07:04:37 +0700

    <update>[article-detail:description] mengubah description untuk mengembalikan nilai menjadi sebuah dom

--- src/Scrapers/articles/FetchArticleDetail.js
+++ src/Scrapers/articles/FetchArticleDetail.js
@@ -5,25 +5,47 @@ const fetchArticleDetail = async (req, res, response) => {
         const $ = cheerio.load(response.data);
         const element = $('.content');
 
-        let title, thumbs, author, published, description, filtered;
+        let title, thumbnail, author, published, description, filtered = [];
         let article_object = {};
         title = element.find('._article-header').find('.title').text();
         author = element.find('.info').find('.author').text().split('|');
         published = author[1].trim();
         author = author[0].trim();
-        thumbs = element.find('picture.thumbnail').find('img').attr('data-src');
+        thumbnail = element.find('picture.thumbnail').find('img').attr('src') || '';
+        
+        element.find('._rich-content > *').each((i, e) => {
+    const allowedHtmlTags = [
+        'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol'
+    ];
 
-        element.find('._rich-content').each((i, e) => {
-            const checkImg = new RegExp('^<img')
-            description = $(e).text().trim().split('\n')
-            filtered = description.filter((e) => !checkImg.test(e))
-        });
+    $(e).find('noscript, figure').remove();
+
+    const tagName = $(e).get(0).tagName.toLowerCase();
+
+    if (allowedHtmlTags.includes(tagName)) {
+        if ($(e).find("a").length > 0) {
+            $(e).find("a").each((j, a) => {
+                const newElement = $('<span>');
+                const content = $(a).text();
+                newElement.text(content);
+                $(a).replaceWith(newElement);
+            });
+        }
+
+        if ($(e).text().replace(/\n/g, '').trim().length >= 1) {
+            const newElement = $(`<${tagName}>`);
+            const content = $(e).html();
+            newElement.html(content);
+            filtered.push($.html(newElement));
+        }
+    }
+});
 
         article_object.title = title.trim();
-        article_object.thumb = thumbs.trim();
+        article_object.thumb = thumbnail.trim();
         article_object.author = author;
         article_object.date_published = published;
-        article_object.description = filtered;
+        article_object.description = filtered.join("");
 
         res.json({
             method: req.method,
